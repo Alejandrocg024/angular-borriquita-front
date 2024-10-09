38f1cc083525a509c714953180fe4f49f7c9c534
Alejandro Campano <alejandrocg024@gmail.com>
2024-10-10 00:51:52 +0200

    1.1.4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 
 export const environments = {
+  // baseUrl: 'https://node-borriquita-back-production.up.railway.app/api',
   baseUrl: 'http://localhost:3000/api',
   stripeAPIKey: 'pk_test_51Q6DoEEO2OO2pOPZvhME2N2gQKvyTKW16nkMikP4K8xU1FXGqHhb67Lw8V3aB2i2ljIzJGp5QA6llltyi309dDDC00gBm3h1Hb',
 }
